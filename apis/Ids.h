b4651cf5cf87cc2d9b043cff7064f99610080695
dkumar798 <dinesh_kumar2@comcast.com>
2025-02-18 11:23:54 +0530

    RDKEMW-1023 Add COM-RPC support to Telemetry plugin
    Reason for change: Add COM-RPC support for Telemetry
    Test Procedure: Test Telemetry - All APIs
    Risks: Medium
    Priority: P1
    Signed-off-by:Dineshkumar P dinesh_kumar2@comcast.com

--- apis/Ids.h
+++ apis/Ids.h
@@ -218,7 +218,7 @@ namespace Exchange {
         ID_RUNTIME_MANAGER                           = ID_ENTOS_OFFSET + 0x210,
         ID_RUNTIME_MANAGER_NOTIFICATION              = ID_RUNTIME_MANAGER + 1,
 
-        ID_TELEMETRY                                 = ID_ENTOS_OFFSET + 0x220,
+        ID_TELEMETRY                                 = ID_ENTOS_OFFSET + 0x260,
         ID_TELEMETRY_NOTIFICATION                    = ID_TELEMETRY + 1,
     };
 }
