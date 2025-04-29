20436f76d5fc83833b033fb3b85644c3e0735e6e
Suresh Kumar <suresh_kumar3@comcast.com>
2025-04-29 13:02:19 -0700

    cleanup

--- apis/PackageManager/IPackageImpl.h
+++ apis/PackageManager/IPackageImpl.h
@@ -1,7 +1,10 @@
 #pragma once
 
+// @stubgen:skip
+
 #include <string>
 #include <memory>
+#include <map>
 #include <vector>
 
 namespace packagemanager {
@@ -11,7 +14,6 @@ enum Result : uint8_t {
     FAILED
 };
 
-// @stubgen:omit
 struct ConfigMetaData {
     bool dial;
     bool wanLanAccess;
@@ -22,29 +24,23 @@ struct ConfigMetaData {
     uint32_t userId;
     uint32_t groupId;
     uint32_t dataImageSize;
-
-    bool resourceManagerClientEnabled;
-    std::string dialId;
-    std::string command;
-    uint32_t appType;
-    std::string appPath;
-    std::string runtimePath;
-
-    std::string fireboltVersion;
-    bool enableDebugger;
 };
 
-// @stubgen:omit
+typedef std::pair<std::string, std::string> ConfigMetadataKey;
+typedef std::map<ConfigMetadataKey, ConfigMetaData>  ConfigMetadataArray;
+
 class IPackageImpl {
     public:
     virtual ~IPackageImpl() = default;
 
-    virtual Result Install(const std::string &packageId, const std::string &version, const std::string &fileLocator) = 0;
+    virtual Result Initialize(ConfigMetadataArray &configMetadata) = 0;
+
+    virtual Result Install(const std::string &packageId, const std::string &version, const std::string &fileLocator, ConfigMetaData &configMetadata) = 0;
     virtual Result Uninstall(const std::string &packageId) = 0;
 
     virtual Result GetList(std::string &packageList) = 0;
 
-    virtual Result Lock(const std::string &packageId, const std::string &version, std::string &unpackedPath, ConfigMetaData& configMetadata) = 0;
+    virtual Result Lock(const std::string &packageId, const std::string &version, std::string &unpackedPath, ConfigMetaData &configMetadata) = 0;
     virtual Result Unlock(const std::string &packageId, const std::string &version) = 0;
 
     virtual Result GetLockInfo(const std::string &packageId, const std::string &version, std::string &unpackedPath, bool &locked) = 0;
