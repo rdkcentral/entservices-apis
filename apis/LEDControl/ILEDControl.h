995545c8f962a40ecf57f26b84324cf9a70a9abb
Arun Madhavan <arun_madhavan@comcast.com>
2025-08-29 12:01:00 -0400

    RDKEMW-3789: clean up the ILEDControl header

--- apis/LEDControl/ILEDControl.h
+++ apis/LEDControl/ILEDControl.h
@@ -29,69 +29,40 @@ namespace WPEFramework
         /* @json 1.0.0 @text:keep */
         struct EXTERNAL ILEDControl : virtual public Core::IUnknown
         {
-            // Utility functions for enum-string conversion
-            enum class LEDControlState : uint8_t {
-                LEDSTATE_NONE = 0,                /* @text NONE */ /* @brief Indicates Uninitialized state */
-                LEDSTATE_ACTIVE,                  /* @text ACTIVE */ /* @brief Indicates Active power state */
-                LEDSTATE_STANDBY,                 /* @text STANDBY */ /* @brief Indicates Standby power state */
-                LEDSTATE_WPS_CONNECTING,          /* @text WPS_CONNECTING */ /* @brief Indicates WPS Connecting */
-                LEDSTATE_WPS_CONNECTED,           /* @text WPS_CONNECTED */ /* @brief Indicates WPS Connected */
-                LEDSTATE_WPS_ERROR,               /* @text WPS_ERROR */ /* @brief Indicates WPS Error */
-                LEDSTATE_FACTORY_RESET,           /* @text FACTORY_RESET */ /* @brief Indicates Factory Reset */
-                LEDSTATE_USB_UPGRADE,             /* @text USB_UPGRADE */ /* @brief Indicates USB Upgrade */
-                LEDSTATE_SOFTWARE_DOWNLOAD_ERROR, /* @text SOFTWARE_DOWNLOAD_ERROR */ /* @brief Indicates Software Download Error */
-                LEDSTATE_UNKNOWN
-            };
-
-            static const char* LEDControlStateToString(LEDControlState state)
-            {
-                switch (state) {
-                    case LEDControlState::LEDSTATE_NONE: return "NONE";
-                    case LEDControlState::LEDSTATE_ACTIVE: return "ACTIVE";
-                    case LEDControlState::LEDSTATE_STANDBY: return "STANDBY";
-                    case LEDControlState::LEDSTATE_WPS_CONNECTING: return "WPS_CONNECTING";
-                    case LEDControlState::LEDSTATE_WPS_CONNECTED: return "WPS_CONNECTED";
-                    case LEDControlState::LEDSTATE_WPS_ERROR: return "WPS_ERROR";
-                    case LEDControlState::LEDSTATE_FACTORY_RESET: return "FACTORY_RESET";
-                    case LEDControlState::LEDSTATE_USB_UPGRADE: return "USB_UPGRADE";
-                    case LEDControlState::LEDSTATE_SOFTWARE_DOWNLOAD_ERROR: return "DOWNLOAD_ERROR";
-                    default: return "UNKNOWN";
-                }
-            }
-
-            static LEDControlState StringToLEDControlState(const std::string& str)
-            {
-                if (str == "NONE") return LEDControlState::LEDSTATE_NONE;
-                if (str == "ACTIVE") return LEDControlState::LEDSTATE_ACTIVE;
-                if (str == "STANDBY") return LEDControlState::LEDSTATE_STANDBY;
-                if (str == "WPS_CONNECTING") return LEDControlState::LEDSTATE_WPS_CONNECTING;
-                if (str == "WPS_CONNECTED") return LEDControlState::LEDSTATE_WPS_CONNECTED;
-                if (str == "WPS_ERROR") return LEDControlState::LEDSTATE_WPS_ERROR;
-                if (str == "FACTORY_RESET") return LEDControlState::LEDSTATE_FACTORY_RESET;
-                if (str == "USB_UPGRADE") return LEDControlState::LEDSTATE_USB_UPGRADE;
-                if (str == "DOWNLOAD_ERROR") return LEDControlState::LEDSTATE_SOFTWARE_DOWNLOAD_ERROR;
-                return LEDControlState::LEDSTATE_UNKNOWN;
-            }
-
             enum { ID = ID_LEDCONTROL };
 
             using IStringIterator = RPC::IIteratorType<string, RPC::ID_STRINGITERATOR>;
 
+            virtual ~ILEDControl() = default;
+
+            enum LEDControlState : uint8_t {
+                LEDSTATE_NONE = 0,       /* @text NONE @brief TRISTATE or not in a defined state */
+                LEDSTATE_ACTIVE,         /* @text ACTIVE @brief Indicates Active power state */
+                LEDSTATE_STANDBY,        /* @text STANDBY @brief Indicates Standby power state */
+                LEDSTATE_WPS_CONNECTING, /* @text WPS_CONNECTING @brief Indicates WPS Connecting */
+                LEDSTATE_WPS_CONNECTED,  /* @text WPS_CONNECTED @brief Indicates WPS Connected */
+                LEDSTATE_WPS_ERROR,      /* @text WPS_ERROR @brief Indicates WPS Error */
+                LEDSTATE_FACTORY_RESET,  /* @text FACTORY_RESET @brief Indicates Factory Reset */
+                LEDSTATE_USB_UPGRADE,    /* @text USB_UPGRADE @brief Indicates USB Upgrade */
+                LEDSTATE_DOWNLOAD_ERROR, /* @text DOWNLOAD_ERROR @brief Indicates Software Download Error */
+                LEDSTATE_MAX             /* @text MAX @brief Indicates Maximum State */
+            };
+
             // @text getSupportedLEDStates
-            // @brief Returns all the LED states supported by the platform
-            // @param supportedLEDStates - out - string [] of supported LED states. e.g. "['NONE', 'ACTIVE', 'STANDBY']"
-            // @param success - out - boolean
+            // @details Returns all the LED states supported by the platform
+            // @param supportedLEDStates: string [] of supported LED states. e.g. "['NONE', 'ACTIVE', 'STANDBY']"
+            // @param success: boolean
             virtual Core::hresult GetSupportedLEDStates(IStringIterator*& supportedLEDStates /* @out */, bool& success /* @out */) = 0;
 
             // @text getLEDState
             // @brief Returns current LED state. e.g. "ACTIVE"
-            // @param state - out - LEDControlState
+            // @param state: LEDControlState
             virtual Core::hresult GetLEDState(LEDControlState& state /* @out */) = 0;
 
             // @text setLEDState
             // @brief Change the device LED state to one mentioned in the input argument.
-            // @param state - in - LEDControlState. e.g. "FACTORY_RESET"
-            // @param success - out - boolean
+            // @param state: LEDControlState. e.g. "FACTORY_RESET"
+            // @param success: boolean
             virtual Core::hresult SetLEDState(const LEDControlState& state, bool& success /* @out */) = 0;
         };
     } // namespace Exchange
