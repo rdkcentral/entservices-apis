7389e6969d1c4f743c3dd96b8a667c31b9dec2a2
Arun Madhavan <arun_madhavan@comcast.com>
2025-08-31 01:03:17 -0400

    RDKEMW-3789: update ILEDControl with documentation tags

--- apis/LEDControl/ILEDControl.h
+++ apis/LEDControl/ILEDControl.h
@@ -49,18 +49,18 @@ namespace WPEFramework
             };
 
             // @text getSupportedLEDStates
-            // @details Returns all the LED states supported by the platform
-            // @param supportedLEDStates: string [] of supported LED states. e.g. "['NONE', 'ACTIVE', 'STANDBY']"
+            // @details Returns the list of LED states that are actually supported by the platform at runtime. Possible values include `NONE`, `ACTIVE`, `STANDBY`, `WPS_CONNECTING`, `WPS_CONNECTED`, `WPS_ERROR`, `FACTORY_RESET`, `USB_UPGRADE` and `DOWNLOAD_ERROR`.
+            // @param supportedLEDStates: string [] of supported LED states. e.g. "['ACTIVE', 'STANDBY', 'WPS_CONNECTING', 'WPS_CONNECTED', 'WPS_ERROR', 'FACTORY_RESET', 'USB_UPGRADE', 'DOWNLOAD_ERROR']"
             // @param success: boolean
             virtual Core::hresult GetSupportedLEDStates(IStringIterator*& supportedLEDStates /* @out */, bool& success /* @out */) = 0;
 
             // @text getLEDState
-            // @brief Returns current LED state. e.g. "ACTIVE"
+            // @brief Retrieves current state of the LED. e.g. "WPS_CONNECTING"
             // @param state: LEDControlState
             virtual Core::hresult GetLEDState(LEDControlState& state /* @out */) = 0;
 
             // @text setLEDState
-            // @brief Change the device LED state to one mentioned in the input argument.
+            // @brief Changes the device LED state to mentioned input state.
             // @param state: LEDControlState. e.g. "FACTORY_RESET"
             // @param success: boolean
             virtual Core::hresult SetLEDState(const LEDControlState& state, bool& success /* @out */) = 0;
