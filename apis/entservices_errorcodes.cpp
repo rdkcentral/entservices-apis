8d219c0e83c3b08c85260b92c314f1e00da57812
ssitar583 <soundaryaa_sitaram@comcast.com>
2025-09-08 10:21:22 -0400

    Update entservices_errorcodes.cpp

--- apis/entservices_errorcodes.cpp
+++ apis/entservices_errorcodes.cpp
@@ -1,7 +1,7 @@
 #include "entservices_errorcodes.h"
 
 #define X(name, string) string,
-const char *error_strings[] = {
+const char* const error_strings[] = {
     "Error base",
     ERROR_LIST
     "Unknown error code"
