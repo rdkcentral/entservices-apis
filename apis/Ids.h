36e41e13a71791d7c50f0928b7143492f9a7310b
Ahearn, Peter <Peter_Ahearn@cable.comcast.com>
2025-09-16 09:38:01 -0400

    AVInput COM-RPC Support: Formatting

--- apis/Ids.h
+++ apis/Ids.h
@@ -287,17 +287,17 @@ namespace Exchange {
         ID_SHARED_STORAGE_LIMIT_INSPECTOR            = ID_SHARED_STORAGE + 6,
         ID_SHARED_STORAGE_CACHE                      = ID_SHARED_STORAGE + 7,
 
-        ID_HDMI_CEC_SINK                              = ID_ENTOS_OFFSET + 0x3C0,
-        ID_HDMI_CEC_SINK_ACTIVE_PATH_ITERATOR         = ID_HDMI_CEC_SINK + 1,
-        ID_HDMI_CEC_SINK_DEVICE_LIST_ITERATOR         = ID_HDMI_CEC_SINK + 2,
-        ID_HDMI_CEC_SINK_NOTIFICATION                 = ID_HDMI_CEC_SINK + 3,
+        ID_HDMI_CEC_SINK                               = ID_ENTOS_OFFSET + 0x3C0,
+        ID_HDMI_CEC_SINK_ACTIVE_PATH_ITERATOR           = ID_HDMI_CEC_SINK + 1,
+        ID_HDMI_CEC_SINK_DEVICE_LIST_ITERATOR           = ID_HDMI_CEC_SINK + 2,
+        ID_HDMI_CEC_SINK_NOTIFICATION                   = ID_HDMI_CEC_SINK + 3,
 
         ID_MIRACAST_SERVICE                          = ID_ENTOS_OFFSET + 0x3D0,
         ID_MIRACAST_SERVICE_NOTIFICATION             = ID_MIRACAST_SERVICE + 1,
         ID_MIRACAST_PLAYER                           = ID_MIRACAST_SERVICE + 2,
         ID_MIRACAST_PLAYER_NOTIFICATION              = ID_MIRACAST_SERVICE + 3,
         ID_MIRACAST_PLAYER_ENV_ARGUMENTS_ITERATOR    = ID_MIRACAST_SERVICE + 4,
-		
+
         ID_FRONT_PANEL                              = ID_ENTOS_OFFSET + 0x3E0,
         ID_FRONT_PANEL_LIGHTS_LIST_ITERATOR         = ID_FRONT_PANEL + 1,
         ID_FRONT_PANEL_BLINK_INFO_LIST_ITERATOR     = ID_FRONT_PANEL + 2,
