3c69a6917ce6087d325df63a3cbeb76ae662f736
sajilal711 <shreyas.sajilal@gmail.com>
2025-09-17 14:49:26 +0530

    RDKEMW-6787: [AI2.0] Create IPreInstallerManager interface
    -fix for review comments

--- apis/PreinstallManager/IPreinstallManager.h
+++ apis/PreinstallManager/IPreinstallManager.h
@@ -26,64 +26,11 @@
 namespace WPEFramework {
 namespace Exchange {
 
-#ifndef RUNTIME_CONFIG
-    struct RuntimeConfig {
-        bool dial;
-        bool wanLanAccess;
-        bool thunder;
-        int32_t systemMemoryLimit;
-        int32_t gpuMemoryLimit;
-        std::string envVariables;
-        uint32_t userId;
-        uint32_t groupId;
-        uint32_t dataImageSize;
-
-        bool resourceManagerClientEnabled;
-        std::string dialId;
-        std::string command;
-        std::string appType;
-        std::string appPath;
-        std::string runtimePath;
-
-        std::string logFilePath;
-        uint32_t logFileMaxSize;
-        std::string logLevels;          //json array of strings
-        bool mapi;
-        std::string fkpsFiles;          //json array of strings
-
-        std::string fireboltVersion;
-        bool enableDebugger;
-    };
-    #define RUNTIME_CONFIG
-#endif
-
 // @json 1.0.0 @text:keep
 struct EXTERNAL IPreinstallManager : virtual public Core::IUnknown {
   enum { ID = ID_PREINSTALL_MANAGER };
 
-  enum PreinstallState : uint8_t {
-          PREINSTALL_STATE_UNKNOWN         = 0   /* @text PREINSTALL_STATE_UNKNOWN */,      //required?
-          PREINSTALL_STATE_INSTALLED       = 1   /* @text PREINSTALL_STATE_INSTALLED */,
-          PREINSTALL_STATE_INSTALL_FAILURE = 2   /* @text PREINSTALL_STATE_INSTALL_FAILURE */
-      };
 
-  enum PreinstallFailReason : uint8_t {
-          PREINSTALL_FAIL_REASON_NONE                           = 0   /* @text PREINSTALL_FAIL_REASON_NONE */,
-          PREINSTALL_FAIL_REASON_UNKNOWN                        = 1   /* @text PREINSTALL_FAIL_REASON_UNKNOWN */,
-          PREINSTALL_FAIL_REASON_SIGNATURE_VERIFICATION_FAILURE = 2   /* @text PREINSTALL_FAIL_REASON_SIGNATURE_VERIFICATION_FAILURE */,
-          PREINSTALL_FAIL_REASON_PERSISTENCE_FAILURE            = 3   /* @text PREINSTALL_FAIL_REASON_PERSISTENCE_FAILURE */
-        };
-
-
-    struct AppInstallInfo {
-        string packageId;
-        string version;
-        PreinstallState state;
-        PreinstallFailReason failReason;
-    };
-
-
-    // using IAppInstallInfoIterator = RPC::IIteratorType<AppInstallInfo, ID_PREINSTALL_MANAGER_INSTALL_INFO_ITERATOR>;
 
   // @event
   struct EXTERNAL INotification : virtual public Core::IUnknown {
@@ -92,8 +39,8 @@ struct EXTERNAL IPreinstallManager : virtual public Core::IUnknown {
 
     // @text onAppInstallationStatus
     // @brief Emitted when the installation of a preinstalled app succeeds or fails.
-    // @param appInstallInfoArray: Output installation status details as string object
-    virtual void OnAppInstallationStatus(const string& jsonresponse) {};
+    // @param jsonresponse: Output installation status details as string object
+    virtual void OnAppInstallationStatus(const string& jsonresponse /* @opaque */) {};
   };
 
   /** Register notification interface */
