a9eb648f88f31801f2ef73b151d1f817e948b841
pkumbh631 <punam_kumbhar@comcast.com>
2025-10-17 21:27:13 +0000

    RDKEMW-9458: Add New Plugins for AppGateway

---
+++ apis/AppNotifications/IAppNotifications.h
@@ -0,0 +1,106 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2024 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "Module.h"
+
+// @stubgen:include <com/IIteratorType.h>
+
+namespace WPEFramework
+{
+    namespace Exchange
+    {
+        // @json 1.0.0 @text:keep
+        struct EXTERNAL IAppNotifications : virtual public Core::IUnknown
+        {
+            enum
+            {
+                ID = ID_APP_NOTIFICATIONS
+            };
+
+            struct Context
+            {
+                int requestId;       // @brief Unique identifier for the request.
+                uint32_t connectionId; // @brief guid for the execution/session context.
+                string appId;        // @brief Application identifier (Firebolt appId).
+                string origin;      // @brief Origin of the request (e.g., org.rdk.AppGateway or org.rdk.LaunchDelegate).
+
+                bool operator==(const Context& other) const {
+                    return this->requestId == other.requestId && this->connectionId == other.connectionId && this->appId == other.appId;
+                }
+            };
+
+            struct EXTERNAL Error
+            {
+                int code;       // @brief Error code.
+                string message; // @brief Error message (optional).
+            };
+
+            // @text subscribe
+            // @brief Thunder register/unregister for Firebolt subsciption requests
+            // @param context: Execution context containing requestId, connectionId, appId
+            // @param register: boolean
+            // @param module: the thunder plugin to subscribe
+            // @param event: the event to subscribe
+            // @returns Core::hresult
+            virtual Core::hresult Subscribe(const Context &context /* @in */,
+                                            bool listen /* @in */,
+                                            const string &module /* @in */,
+                                            const string &event /* @in */) = 0;
+
+            // @text emit
+            // @brief Dispatch event for a given registration, if appId is provided the dispatch happens for a given App.
+            // @param event: the event to emit
+            // @param payload: the payload to emit
+            // @param appId (optional): the appId to emit the event for, if empty the event is emitted for all Apps
+            // @returns Core::hresult
+            virtual Core::hresult Emit(const string &event /* @in */,
+                                       const string &payload /* @in @opaque */,
+                                       const string &appId /* @in */) = 0;
+
+            // @text Cleanup
+            // @brief Cleanup any context which has a given connectionId for a given origin
+            // @param connectionId: connection id for a given context
+            // @param origin: origin of the context
+            // @returns Core::hresult
+            virtual Core::hresult Cleanup(const uint32_t connectionId /* @in */, const string &origin /* @in */) = 0;
+
+        };
+
+        // @text:keep
+        struct EXTERNAL IAppNotificationHandlerInternal : virtual public Core::IUnknown
+        {
+            enum
+            {
+                ID = ID_APP_NOTIFICATIONS_HANDLER_INTERNAL
+            };
+
+            // @json:omit
+            // @text handleAppEventNotifier
+            // @brief Handle AppEvent Notfier expectations for a given event
+            // @param event: the event for registration
+            // @param listen: whether to listen
+            // @param status: status to be filled in
+            // @returns Core::hresult
+            virtual Core::hresult HandleAppEventNotifier(const string& event /* @in */, const bool& listen /* @in */, bool& status /* @out */) = 0;
+
+        };
+    } // namespace Exchange
+} // namespace WPEFramework
