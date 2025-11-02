6188cfa98e6de2a57717c47fdccb546c02caa59e
Sathishkumar Deena Kirupakaran <satlead@gmail.com>
2025-11-02 12:12:58 +0000

    Revert "RDKEMW-9458: Fix Whitespaces"
    This reverts commit ea989c3d6e22e08b3d1a933141902cc2456c153e.

--- apis/AppNotifications/IAppNotifications.h
+++ apis/AppNotifications/IAppNotifications.h
@@ -16,8 +16,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #pragma once
+
 #include "Module.h"
+
 namespace WPEFramework
 {
     namespace Exchange
