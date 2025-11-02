bc6ffce27d306672be2f2d75cef956f06ab91eb1
Sathishkumar Deena Kirupakaran <satlead@gmail.com>
2025-11-02 12:16:22 +0000

    Fix: spacing

--- apis/AppGateway/IAppGateway.h
+++ apis/AppGateway/IAppGateway.h
@@ -16,13 +16,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #pragma once
-
 #include "Module.h"
-
 // @stubgen:include <com/IIteratorType.h>
-
 namespace WPEFramework
 {
     namespace Exchange
--- apis/AppNotifications/IAppNotifications.h
+++ apis/AppNotifications/IAppNotifications.h
@@ -16,11 +16,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #pragma once
-
 #include "Module.h"
-
 namespace WPEFramework
 {
     namespace Exchange
