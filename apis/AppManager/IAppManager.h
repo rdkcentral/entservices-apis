98015546091570500107e1a4cfa05d8a3f82722a
Josekutty Kuriakose <Josekutty_Kuriakose@cable.comcast.com>
2025-12-17 20:58:20 +0000

    Added ralfPkgPath variable

--- apis/AppManager/IAppManager.h
+++ apis/AppManager/IAppManager.h
@@ -53,6 +53,7 @@ struct RuntimeConfig
     std::string fireboltVersion;
     bool enableDebugger{false};
     std::string unpackedPath;
+    std::string ralfPkgPath; //Json string containing ralf dependency details.
 };
 #define RUNTIME_CONFIG
 #endif
