56eb3f17d3b2bb9aed27f308cfc2a604305c09c1
naveen-0206 <naveenkumar.t@ltts.com>
2026-01-27 16:09:33 +0530

    RDKEMW-1000 : Add COM-RPC support to UnifiedCASManagement plugin Reason For Change: Adding UnifiedCASManagement interface header Changes Test procedure : Test UnifiedCASManagement Priority: P1
    Signed-off-by: naveen-0206 <naveenkumar.t@ltts.com>

---
+++ apis/UnifiedCASManagement/IUnifiedCASManagement.h
@@ -0,0 +1,98 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2026 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+#include "Module.h"
+
+// @stubgen:include <com/IIteratorType.h>
+
+namespace WPEFramework
+{
+    namespace Exchange
+    {
+        /* @json 1.0.0 @text:keep */
+        struct EXTERNAL IUnifiedCASManagement : virtual public Core::IUnknown 
+        {
+            enum { ID = ID_UNIFIEDCASMANAGEMENT };
+
+            // @event
+            struct EXTERNAL INotification : virtual public Core::IUnknown 
+            {
+                enum { ID = ID_UNIFIEDCASMANAGEMENT_NOTIFICATION };
+
+                // @text data
+                // @brief Sent when the CAS needs to send data to the caller  
+                // @param payload - in - string - Data to transfer. Can be base64 coded if required
+                // @param source - in - string - Origin of the data (PUBLIC or PRIVATE)
+                virtual void Event_data(const string& payload, const string& source) {}
+            };
+
+            // @brief Register a notification callback
+            // @param notification: The notification callback object
+            // @retval Core::ERROR_NONE: Registration successful
+            // @retval Core::ERROR_ALREADY_CONNECTED: Callback already registered
+            virtual Core::hresult Register(IUnifiedCASManagement::INotification* notification) = 0;
+            
+            // @brief Unregister a notification callback
+            // @param notification: The notification callback object to remove
+            // @retval Core::ERROR_NONE: Unregistration successful
+            // @retval Core::ERROR_UNKNOWN_KEY: Callback not found
+            virtual Core::hresult Unregister(IUnifiedCASManagement::INotification* notification) = 0;
+
+            /**********************manage() - start****************************/
+            // @text manage
+            // @brief Manage a well-known CAS (setup CAS management session)
+            // @param mediaurl - in - string - The URL to tune to (tune://, ocap://, http://, https://)
+            // @param mode - in - string - The use of the tune request (MODE_NONE, MODE_LIVE, MODE_RECORD, MODE_PLAYBACK)
+            // @param managementType - in - string - Type of CAS management (MANAGE_NONE, MANAGE_FULL, MANAGE_NO_PSI, MANAGE_NO_TUNER)
+            // @param casinitdata - in - string - CAS specific initdata for the selected media
+            // @param casocdmid - in - string - The well-known OCDM ID of the CAS to use
+            // @param success - out - bool
+            // @retval Core::ERROR_NONE: Operation successful
+            // @retval Core::ERROR_GENERAL: Operation failed
+            virtual Core::hresult Manage(const string& mediaurl, 
+                                       const string& mode, 
+                                       const string& managementType, 
+                                       const string& casinitdata, 
+                                       const string& casocdmid,
+                                       bool& success /* @out */) = 0;
+            /**********************manage() - end******************************/
+
+            /**********************unmanage() - start***************************/
+            // @text unmanage
+            // @brief Destroy a management session
+            // @param success - out - bool
+            // @retval Core::ERROR_NONE: Operation successful
+            // @retval Core::ERROR_GENERAL: Operation failed
+            virtual Core::hresult Unmanage(bool& success /* @out */) = 0;
+            /**********************unmanage() - end*****************************/
+
+            /**********************send() - start*******************************/
+            // @text send 
+            // @brief Sends data to the remote CAS
+            // @param payload - in - string - Data to transfer. Can be base64 coded if required
+            // @param source - in - string - Origin of the data (PUBLIC or PRIVATE)
+            // @param success - out - bool
+            // @retval Core::ERROR_NONE: Operation successful
+            // @retval Core::ERROR_GENERAL: Operation failed
+            virtual Core::hresult Send(const string& payload, const string& source, bool& success /* @out */) = 0;
+            /**********************send() - end*********************************/
+        };
+    } // namespace Exchange
+} // namespace WPEFramework
