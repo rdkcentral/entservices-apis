df8ca607c40eb45d7fdf90aec34086392ceca46e
Josekutty Kuriakose <Josekutty_Kuriakose@cable.comcast.com>
2026-02-16 21:50:39 +0000

    Fixed copilot suggestions.

--- apis/AppManager/IAppManager.h
+++ apis/AppManager/IAppManager.h
@@ -49,7 +49,7 @@ struct RuntimeConfig
     std::string logLevels;          //json array of strings
     bool mapi {false};
     std::string fkpsFiles;          //json array of strings
-    std::string ralfPkgPath;
+    std::string ralfPkgPath;        //Filesystem path holding metadata info of ralf packages
 
     std::string fireboltVersion;
     bool enableDebugger{false};
