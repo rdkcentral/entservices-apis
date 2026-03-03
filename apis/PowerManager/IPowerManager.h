db3fe18fa055666ff4b755e831706471dcbabeda
dkumar798 <dinesh_kumar2@comcast.com>
2026-03-03 19:16:09 +0530

    Update IPowerManager.h

--- apis/PowerManager/IPowerManager.h
+++ apis/PowerManager/IPowerManager.h
@@ -327,9 +327,9 @@ namespace WPEFramework
         // @param delayPeriod: delay in seconds
         virtual Core::hresult DelayPowerModeChangeBy(const uint32_t clientId , const int transactionId , const int delayPeriod ) = 0;
 
-        /** Get the Wakeup Time */
+        /** Get the Wakeup Time in seconds */
         // @text getTimeSinceWakeup
-        // @brief Get the Wakeup Time
+        // @brief Get the Wakeup Time in seconds since the device transitioned to the ON state.
         // @param timeSinceWakeup: Wakeup time in seconds
         // @retval ErrorCode::ERROR_NONE: Indicates success
         // @retval ErrorCode::ERROR_GENERAL: Indicates failure
