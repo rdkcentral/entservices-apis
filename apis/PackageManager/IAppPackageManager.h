5a53cacec3ebc99f30c3823c883d2f6b67f6de0e
Madana Gopal <madanagopal123@gmail.com>
2026-05-04 07:23:16 +0530

    RDKEMW-17636: Add capabilities in runtimeconfig

--- apis/PackageManager/IAppPackageManager.h
+++ apis/PackageManager/IAppPackageManager.h
@@ -31,6 +31,7 @@ namespace Exchange {
         std::string logLevels;          //json array of strings
         bool mapi;
         std::string fkpsFiles;          //json array of strings
+        std::string capabilities;       //comma-separated lowercase runtime capability tokens
         std::string ralfPkgPath;        //Filesystem path holding metadata info of ralf packages
 
         std::string fireboltVersion;
