d3dfc08952950104f460bd39655ca1515bb4ef57
Lukasz Iwan <lukasz_iwan@comcast.com>
2026-06-29 17:08:29 +0200

    feat: Introduce AVMonitor and ResourceMonitor interfaces

--- apis/Ids.h
+++ apis/Ids.h
@@ -356,6 +356,12 @@ namespace Exchange {
         ID_VOICECONTROL                             = ID_ENTOS_OFFSET + 0x540,
         ID_VOICECONTROL_NOTIFICATION                = ID_VOICECONTROL + 1,
 
+        ID_RESOURCEMONITOR                          = ID_ENTOS_OFFSET + 0x550,
+        ID_RESOURCEMONITOR_NOTIFICATION             = ID_RESOURCEMONITOR + 1,
+
+        ID_AVMONITOR                                = ID_ENTOS_OFFSET + 0x560,
+        ID_AVMONITOR_NOTIFICATION                   = ID_AVMONITOR + 1,
+
     }; // enum IDS
 
 } // namespace Exchange
