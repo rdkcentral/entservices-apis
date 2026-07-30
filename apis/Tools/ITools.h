a827cb18c2a69f14dc7c400aeaaf4f9576d795a6
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2026-07-30 13:14:00 -0400

    RDKEMW-21352 : Create Entservices Tools Interface header

--- apis/Tools/ITools.h
+++ apis/Tools/ITools.h
@@ -21,12 +21,96 @@
 
 #include "Module.h"
 
-// @stubgen:include <com/IIteratorType.h>
-
 namespace WPEFramework
 {
     namespace Exchange
     {
+        enum Modifier : uint32_t {
+            NONE = 0 /* @text NONE */,
+            CTRL = 1 /* @text CTRL */,
+            ALT = 2 /* @text ALT */,
+            ALT_CTRL = 3 /* @text ALT_CTRL */,
+            SHIFT = 4 /* @text SHIFT */,
+            SHIFT_CTRL = 5 /* @text SHIFT_CTRL */,
+            SHIFT_ALT = 6 /* @text SHIFT_ALT */,
+            SHIFT_ALT_CTRL = 7 /* @text SHIFT_ALT_CTRL */
+        };
+
+        enum RemoteKeyCode : uint32_t {
+            KED_MENU = 102 /* @text KED_MENU */,              /* KEY_HOME */
+            KED_GUIDE = 102 /* @text KED_GUIDE */,             /* KEY_HOME */
+            KED_INFO = 67 /* @text KED_INFO */,               /* KEY_F9 */
+            KED_STAR = 64 /* @text KED_STAR */,               /* KEY_F6 */
+            KED_TVPOWER = 59 /* @text KED_TVPOWER */,            /* KEY_F1 */
+            KED_INPUTKEY = 185 /* @text KED_INPUTKEY */,          /* KEY_F15 */
+            KED_OK = 352 /* @text KED_OK */,                /* KEY_OK */
+            KED_SELECT = 28 /* @text KED_SELECT */,             /* KEY_ENTER */
+            KED_ENTER = 28 /* @text KED_ENTER */,              /* KEY_ENTER */
+            KED_EXIT = 1 /* @text KED_EXIT */,                /* KEY_ESC */
+            KED_BACK = 1 /* @text KED_BACK */,                /* KEY_ESC */
+            KED_PERIOD = 63 /* @text KED_PERIOD */,             /* KEY_F5 */
+            KED_PUSH_TO_TALK = 66 /* @text KED_PUSH_TO_TALK */,       /* KEY_F8 */
+            KED_POWER = 116 /* @text KED_POWER */,             /* KEY_POWER */
+            KED_CHANNELUP = 103 /* @text KED_CHANNELUP */,         /* KEY_UP, modifier KEY_LEFTCTRL = 29 */
+            KED_CHANNELDOWN = 108 /* @text KED_CHANNELDOWN */,       /* KEY_DOWN, modifier KEY_LEFTCTRL = 29 */
+            KED_VOLUMEUP = 78 /* @text KED_VOLUMEUP */,           /* KEY_KPPLUS */
+            KED_VOLUMEDOWN = 74 /* @text KED_VOLUMEDOWN */,         /* KEY_KPMINUS */
+            KED_MUTE = 55 /* @text KED_MUTE */,               /* KEY_KPASTERISK */
+            KED_DIGIT1 = 2 /* @text KED_DIGIT1 */,              /* KEY_1 */
+            KED_DIGIT2 = 3 /* @text KED_DIGIT2 */,              /* KEY_2 */
+            KED_DIGIT3 = 4 /* @text KED_DIGIT3 */,              /* KEY_3 */
+            KED_DIGIT4 = 5 /* @text KED_DIGIT4 */,              /* KEY_4 */
+            KED_DIGIT5 = 6 /* @text KED_DIGIT5 */,              /* KEY_5 */
+            KED_DIGIT6 = 7 /* @text KED_DIGIT6 */,              /* KEY_6 */
+            KED_DIGIT7 = 8 /* @text KED_DIGIT7 */,              /* KEY_7 */
+            KED_DIGIT8 = 9 /* @text KED_DIGIT8 */,              /* KEY_8 */
+            KED_DIGIT9 = 10 /* @text KED_DIGIT9 */,             /* KEY_9 */
+            KED_DIGIT0 = 11 /* @text KED_DIGIT0 */,             /* KEY_0 */
+            KED_FASTFORWARD = 88 /* @text KED_FASTFORWARD */,        /* KEY_F12 */
+            KED_REWIND = 68 /* @text KED_REWIND */,             /* KEY_F10 */
+            KED_PAUSE = 87 /* @text KED_PAUSE */,              /* KEY_F11 */
+            KED_PLAY = 87 /* @text KED_PLAY */,               /* KEY_F11 */
+            KED_STOP = 31 /* @text KED_STOP */,               /* KEY_S, modifier KEY_LEFTCTRL = 29 */
+            KED_RECORD = 65 /* @text KED_RECORD */,             /* KEY_F7 */
+            KED_ARROWUP = 103 /* @text KED_ARROWUP */,           /* KEY_UP */
+            KED_ARROWDOWN = 108 /* @text KED_ARROWDOWN */,         /* KEY_DOWN */
+            KED_ARROWLEFT = 105 /* @text KED_ARROWLEFT */,         /* KEY_LEFT */
+            KED_ARROWRIGHT = 106 /* @text KED_ARROWRIGHT */,        /* KEY_RIGHT */
+            KED_PAGEUP = 104 /* @text KED_PAGEUP */,            /* KEY_PAGEUP */
+            KED_PAGEDOWN = 109 /* @text KED_PAGEDOWN */,          /* KEY_PAGEDOWN */
+            KED_LAST = 38 /* @text KED_LAST */,               /* KEY_L, modifier KEY_LEFTCTRL = 29 */
+            KED_FAVORITE = 49 /* @text KED_FAVORITE */,           /* KEY_N, modifier KEY_LEFTCTRL = 29 */
+            KED_KEYA = 110 /* @text KED_KEYA */,              /* KEY_INSERT */
+            KED_KEYB = 107 /* @text KED_KEYB */,              /* KEY_END */
+            KED_KEYC = 62 /* @text KED_KEYC */,               /* KEY_F4 */
+            KED_KEYD = 111 /* @text KED_KEYD */,              /* KEY_DELETE */
+            KED_HELP = 60 /* @text KED_HELP */,               /* KEY_F2 */
+            KED_SETUP = 141 /* @text KED_SETUP */,             /* KEY_SETUP */
+            KED_NEXT = 407 /* @text KED_NEXT */,              /* KEY_NEXT */
+            KED_PREVIOUS = 412 /* @text KED_PREVIOUS */,          /* KEY_PREVIOUS */
+            KED_ONDEMAND = 63 /* @text KED_ONDEMAND */,           /* KEY_F5 */
+            KED_POUND = 236 /* @text KED_POUND */,             /* KEY_BATTERY */
+            KED_AUDIO = 193 /* @text KED_AUDIO */,             /* KEY_F23 */
+            KED_CLOSED_CAPTIONING = 194 /* @text KED_CLOSED_CAPTIONING */, /* KEY_F24 */
+            KED_REPLAY = 48 /* @text KED_REPLAY */,             /* KEY_B, modifier KEY_LEFTCTRL = 29 */
+            KED_SEARCH = 61 /* @text KED_SEARCH */,             /* KEY_F3 */
+            KED_RF_PAIR_GHOST = 237 /* @text KED_RF_PAIR_GHOST */,     /* KEY_BLUETOOTH */
+            KED_UNDEFINEDKEY = 240 /* @text KED_UNDEFINEDKEY */       /* KEY_UNKNOWN */
+        };
+
+        struct EXTERNAL ToolsKey {
+            int code /* @text code */ /* @brief Linux key code as defined in linux/include/uapi/linux/input-event-codes.h. The full Linux key code list is supported. */;
+            Modifier modifier /* @text modifier */ /* @brief Key modifier */;
+            uint32_t delay /* @text delay */ /* @brief Seconds to wait before sending the key event */;
+            uint32_t duration /* @text duration */ /* @brief Seconds between key press and key release; use 0 if not required */;
+        };
+
+        struct EXTERNAL RemoteKey {
+            RemoteKeyCode code /* @text code */ /* @brief Remote key code */;
+            uint32_t delay /* @text delay */ /* @brief Seconds to wait before sending the key event */;
+            uint32_t duration /* @text duration */ /* @brief Seconds between key press and key release; use 0 if not required */;
+        };
+
         // @json 1.0.0 @text:keep
         struct EXTERNAL ITools : virtual public Core::IUnknown {
             enum { ID = ID_TOOLS };
@@ -34,81 +118,25 @@ namespace WPEFramework
             // @json:omit
             virtual Core::hresult Configure(PluginHost::IShell* service) = 0;
 
-            // @text generateKey
-            // @brief Simulates key events for a list of key entries. Each entry contains parallel arrays of key codes and
-            //        their corresponding modifier sets, along with timing controls.
-            // @details Supported action to key mapping:
-            // | Action | Linux Key Symbol | Linux Key Code | Modifiers |
-            // | --- | --- | ---: | --- |
-            // | Menu | KEY_HOME | 102 | - |
-            // | Guide | KEY_HOME | 102 | - |
-            // | Info | KEY_F9 | 67 | - |
-            // | Star | KEY_F6 | 64 | - |
-            // | TV Power | KEY_F1 | 59 | - |
-            // | Input | KEY_F15 | 185 | - |
-            // | OK | KEY_OK | 352 | - |
-            // | Select | KEY_ENTER | 28 | - |
-            // | Enter | KEY_ENTER | 28 | - |
-            // | Exit | KEY_ESC | 1 | - |
-            // | Back | KEY_ESC | 1 | - |
-            // | Period | KEY_F5 | 63 | - |
-            // | Push To Talk | KEY_F8 | 66 | - |
-            // | Power | KEY_POWER | 116 | - |
-            // | Channel Up | KEY_UP | 103 | ctrl |
-            // | Channel Down | KEY_DOWN | 108 | ctrl |
-            // | Volume Up | KEY_KPPLUS | 78 | - |
-            // | Volume Down | KEY_KPMINUS | 74 | - |
-            // | Mute | KEY_KPASTERISK | 55 | - |
-            // | Digit 1 | KEY_1 | 2 | - |
-            // | Digit 2 | KEY_2 | 3 | - |
-            // | Digit 3 | KEY_3 | 4 | - |
-            // | Digit 4 | KEY_4 | 5 | - |
-            // | Digit 5 | KEY_5 | 6 | - |
-            // | Digit 6 | KEY_6 | 7 | - |
-            // | Digit 7 | KEY_7 | 8 | - |
-            // | Digit 8 | KEY_8 | 9 | - |
-            // | Digit 9 | KEY_9 | 10 | - |
-            // | Digit 0 | KEY_0 | 11 | - |
-            // | Fast Forward | KEY_F12 | 88 | - |
-            // | Rewind | KEY_F10 | 68 | - |
-            // | Pause | KEY_F11 | 87 | - |
-            // | Play | KEY_F11 | 87 | - |
-            // | Stop | KEY_S | 31 | ctrl |
-            // | Record | KEY_F7 | 65 | - |
-            // | Arrow Up | KEY_UP | 103 | - |
-            // | Arrow Down | KEY_DOWN | 108 | - |
-            // | Arrow Left | KEY_LEFT | 105 | - |
-            // | Arrow Right | KEY_RIGHT | 106 | - |
-            // | Page Up | KEY_PAGEUP | 104 | - |
-            // | Page Down | KEY_PAGEDOWN | 109 | - |
-            // | Last | KEY_L | 38 | ctrl |
-            // | Favorite | KEY_N | 49 | ctrl |
-            // | Key A | KEY_INSERT | 110 | - |
-            // | Key B | KEY_END | 107 | - |
-            // | Key C | KEY_F4 | 62 | - |
-            // | Key D | KEY_DELETE | 111 | - |
-            // | Help | KEY_F2 | 60 | - |
-            // | Setup | KEY_SETUP | 141 | - |
-            // | Next | KEY_NEXT | 407 | - |
-            // | Previous | KEY_PREVIOUS | 412 | - |
-            // | On Demand | KEY_F5 | 63 | - |
-            // | Pound | KEY_BATTERY | 236 | - |
-            // | Audio | KEY_F23 | 193 | - |
-            // | Closed Captioning | KEY_F24 | 194 | - |
-            // | Replay | KEY_B | 48 | ctrl |
-            // | Search | KEY_F3 | 61 | - |
-            // | RF Pair Ghost | KEY_BLUETOOTH | 237 | - |
-            // | Undefined | KEY_UNKNOWN | 240 | - |
-            // @param keys: JSON array of key entry objects. Each object contains:
-            //   keyCode (array of Linux key codes),
-            //   modifiers (array of arrays of modifier strings, where modifiers[i] applies to keyCode[i]; supported values: "ctrl", "alt", "shift"),
-            //   delay (number, seconds to wait before sending the key event),
-            //   duration (number, optional, seconds between key press and key release; omit or 0 if not used)
+            // @text generateKeys
+            // @brief Generates local key events for each key entry.
+            // @param keys: List of keys to generate in order. Each entry supports delay and optional duration.
             // @param success: Indicates whether the operation was successful
+            // @details Implementations should generate key press and key release events via /dev/uinput and honor delay/duration values.
             // @retval Core::ERROR_NONE: Keys generated successfully
-            // @retval Core::ERROR_GENERAL: Failed to generate keys
-            // @retval Core::ERROR_INVALID_INPUT_LENGTH: The keys list is empty or invalid
-            virtual Core::hresult GenerateKey(const string& keys /* @opaque */, bool& success /* @out */) = 0;
+            // @retval Core::ERROR_GENERAL: Failed to generate one or more key events
+            // @retval Core::ERROR_INVALID_INPUT_LENGTH: Input key list is empty
+            virtual Core::hresult GenerateKeys(const std::vector<ToolsKey>& keys /* @restrict:100 */, bool& success /* @out */) = 0;
+
+            // @text generateRemoteKeys
+            // @brief Generates remote key events for each key entry.
+            // @param keys: List of remote keys to generate in order. Each entry supports delay and optional duration.
+            // @param success: Indicates whether the operation was successful
+            // @details Implementations should generate key press and key release events via /dev/uinput and honor delay/duration values.
+            // @retval Core::ERROR_NONE: Remote keys generated successfully
+            // @retval Core::ERROR_GENERAL: Failed to generate one or more remote key events
+            // @retval Core::ERROR_INVALID_INPUT_LENGTH: Input key list is empty
+            virtual Core::hresult GenerateRemoteKeys(const std::vector<RemoteKey>& keys /* @restrict:100 */, bool& success /* @out */) = 0;
 
         };
 } // namespace Exchange
--- docs/apis/Tools.md
+++ docs/apis/Tools.md
@@ -54,85 +54,18 @@ The following methods are provided by the ITools Interface:
 
 | Method | Description |
 | :-------- | :-------- |
-| [generateKey](#generateKey) | Simulates key events for a list of key entries. Each entry contains parallel arrays of key codes and their corresponding modifier sets, along with timing controls. |
+| [generateKeys](#generateKeys) | Generates local key events for each key entry. |
+| [generateRemoteKeys](#generateRemoteKeys) | Generates remote key events for each key entry. |
 
-<a id="generateKey"></a>
-## *generateKey*
+<a id="generateKeys"></a>
+## *generateKeys*
 
-Supported action to key mapping:
-
-| Action | Linux Key Symbol | Linux Key Code | Modifiers |
-| :-------- | :-------- | :-------- | :-------- |
-| Menu | KEY_HOME | 102 | - |
-| Guide | KEY_HOME | 102 | - |
-| Info | KEY_F9 | 67 | - |
-| Star | KEY_F6 | 64 | - |
-| TV Power | KEY_F1 | 59 | - |
-| Input | KEY_F15 | 185 | - |
-| OK | KEY_OK | 352 | - |
-| Select | KEY_ENTER | 28 | - |
-| Enter | KEY_ENTER | 28 | - |
-| Exit | KEY_ESC | 1 | - |
-| Back | KEY_ESC | 1 | - |
-| Period | KEY_F5 | 63 | - |
-| Push To Talk | KEY_F8 | 66 | - |
-| Power | KEY_POWER | 116 | - |
-| Channel Up | KEY_UP | 103 | ctrl |
-| Channel Down | KEY_DOWN | 108 | ctrl |
-| Volume Up | KEY_KPPLUS | 78 | - |
-| Volume Down | KEY_KPMINUS | 74 | - |
-| Mute | KEY_KPASTERISK | 55 | - |
-| Digit 1 | KEY_1 | 2 | - |
-| Digit 2 | KEY_2 | 3 | - |
-| Digit 3 | KEY_3 | 4 | - |
-| Digit 4 | KEY_4 | 5 | - |
-| Digit 5 | KEY_5 | 6 | - |
-| Digit 6 | KEY_6 | 7 | - |
-| Digit 7 | KEY_7 | 8 | - |
-| Digit 8 | KEY_8 | 9 | - |
-| Digit 9 | KEY_9 | 10 | - |
-| Digit 0 | KEY_0 | 11 | - |
-| Fast Forward | KEY_F12 | 88 | - |
-| Rewind | KEY_F10 | 68 | - |
-| Pause | KEY_F11 | 87 | - |
-| Play | KEY_F11 | 87 | - |
-| Stop | KEY_S | 31 | ctrl |
-| Record | KEY_F7 | 65 | - |
-| Arrow Up | KEY_UP | 103 | - |
-| Arrow Down | KEY_DOWN | 108 | - |
-| Arrow Left | KEY_LEFT | 105 | - |
-| Arrow Right | KEY_RIGHT | 106 | - |
-| Page Up | KEY_PAGEUP | 104 | - |
-| Page Down | KEY_PAGEDOWN | 109 | - |
-| Last | KEY_L | 38 | ctrl |
-| Favorite | KEY_N | 49 | ctrl |
-| Key A | KEY_INSERT | 110 | - |
-| Key B | KEY_END | 107 | - |
-| Key C | KEY_F4 | 62 | - |
-| Key D | KEY_DELETE | 111 | - |
-| Help | KEY_F2 | 60 | - |
-| Setup | KEY_SETUP | 141 | - |
-| Next | KEY_NEXT | 407 | - |
-| Previous | KEY_PREVIOUS | 412 | - |
-| On Demand | KEY_F5 | 63 | - |
-| Pound | KEY_BATTERY | 236 | - |
-| Audio | KEY_F23 | 193 | - |
-| Closed Captioning | KEY_F24 | 194 | - |
-| Replay | KEY_B | 48 | ctrl |
-| Search | KEY_F3 | 61 | - |
-| RF Pair Ghost | KEY_BLUETOOTH | 237 | - |
-| Undefined | KEY_UNKNOWN | 240 | - |
+Implementations should generate key press and key release events via /dev/uinput and honor delay/duration values.
 
 ### Events Triggered
 None
-
 ### Parameters
-| Name | Type | Description |
-| :-------- | :-------- | :-------- |
-| params | object |  |
-| params.keys | string | JSON array of key entry objects. Each object contains: keyCode (array of Linux key codes), modifiers (array of arrays of modifier strings, where modifiers[i] applies to keyCode[i]; supported values: "ctrl", "alt", "shift"), delay (number, seconds to wait before sending the key event), duration (number, optional, seconds between key press and key release; omit or 0 if not used) |
-
-
+This method takes no parameters.
 ### Results
 | Name | Type | Description |
 | :-------- | :-------- | :-------- |
@@ -148,10 +81,7 @@ None
 {
     "jsonrpc": 2.0,
     "id": 0,
-    "method": "org.rdk.Tools.generateKey",
-    "params": {
-        "keys": ""
-    }
+    "method": "org.rdk.Tools.generateKeys"
 }
 ```
 
@@ -159,9 +89,8 @@ None
 #### CURL Command
 
 ```curl
-curl -d '{  "jsonrpc": "2.0",  "id": 42,  "method": "org.rdk.Tools.generateKey",  "params": {    "keys": [      {        "keyCode": 11,        "modifiers": ["ctrl", "alt"],        "delay": 0.5,        "duration": 0.1      }    ]  }}' http://127.0.0.1:9998/jsonrpc
+curl  -v -H "Content-Type: application/json" -d '{  "jsonrpc": "2.0",  "id": 42,  "method": "org.rdk.Tools.generateKeys",  "params": {"keys" : [{"code": 1,"modifier": "CTRL","delay": 0,"duration": 0}] } }' http://127.0.0.1:9998/jsonrpc
 ```
-
 
 
 #### Response
@@ -185,7 +114,7 @@ curl -d '{  "jsonrpc": "2.0",  "id": 42,  "method": "org.rdk.Tools.generateKey",
     "id": 0,
     "error": {
         "code": 1,
-        "message": "Failed to generate keys"
+        "message": "Failed to generate one or more key events"
     }
 }
 ```
@@ -199,7 +128,83 @@ curl -d '{  "jsonrpc": "2.0",  "id": 42,  "method": "org.rdk.Tools.generateKey",
     "id": 0,
     "error": {
         "code": 14,
-        "message": "The keys list is empty or invalid"
+        "message": "Input key list is empty"
     }
 }
 ```
+<a id="generateRemoteKeys"></a>
+## *generateRemoteKeys*
+
+Implementations should generate key press and key release events via /dev/uinput and honor delay/duration values.
+
+### Events Triggered
+None
+### Parameters
+This method takes no parameters.
+### Results
+| Name | Type | Description |
+| :-------- | :-------- | :-------- |
+| result | object |  |
+| result.success | bool | Indicates whether the operation was successful |
+
+### Examples
+
+
+#### Request
+
+```json
+{
+    "jsonrpc": 2.0,
+    "id": 1,
+    "method": "org.rdk.Tools.generateRemoteKeys"
+}
+```
+
+
+#### CURL Command
+
+```curl
+curl -H 'content-type:text/plain;' --data-binary '{"jsonrpc": 2.0, "id": 1, "method": "org.rdk.Tools.generateRemoteKeys"}' http://127.0.0.1:9998/jsonrpc
+```
+
+
+#### Response
+
+```json
+{
+    "jsonrpc": 2.0,
+    "id": 1,
+    "result": {
+        "success": true
+    }
+}
+```
+
+
+#### Error Response (Core::ERROR_GENERAL)
+
+```json
+{
+    "jsonrpc": 2.0,
+    "id": 1,
+    "error": {
+        "code": 1,
+        "message": "Failed to generate one or more remote key events"
+    }
+}
+```
+
+
+#### Error Response (Core::ERROR_INVALID_INPUT_LENGTH)
+
+```json
+{
+    "jsonrpc": 2.0,
+    "id": 1,
+    "error": {
+        "code": 14,
+        "message": "Input key list is empty"
+    }
+}
+```
+
