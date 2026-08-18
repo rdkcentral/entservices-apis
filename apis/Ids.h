aee636a2fca135886cefd25157db539f70d2878e
shashank.kumar@sky.uk <shashank.kumar@sky.uk>
2026-08-18 16:09:25 +0100

    RDKEMW-22020: Enable or Disable app specific audio confgiuration

--- apis/Ids.h
+++ apis/Ids.h
@@ -357,6 +357,8 @@ namespace Exchange {
         ID_BACKUP_MANAGER                           = ID_ENTOS_OFFSET + 0x510,
         ID_BACKUP_PROVIDER                          = ID_BACKUP_MANAGER + 1,
 
+        ID_AUDIO_OUTPUT                             = ID_ENTOS_OFFSET + 0x560,
+        ID_AUDIO_OUTPUT_NOTIFICATION                = ID_AUDIO_OUTPUT + 1,
     }; // enum IDS
 
 } // namespace Exchange
