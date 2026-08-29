ae24fd3d12ea7a5d502872e25b1696f98981663e
dkumar798 <dinesh_kumar2@comcast.com>
2026-08-29 11:37:29 +0530

    RDKEMW-24176: Device turn on from deep sleep due to FFV during maintenance window

--- apis/SystemServices/ISystemServices.h
+++ apis/SystemServices/ISystemServices.h
@@ -167,18 +167,24 @@ namespace WPEFramework
                 bool success /* @brief Whether the request succeeded */;
             };
 
-             struct EXTERNAL WakeupSources {
-                bool voice /* @text WAKEUPSRC_VOICE */ /* @brief Voice Wake up */;
-                bool presenceDetection /* @text WAKEUPSRC_PRESENCE_DETECTION */ /* @brief Presence detection wake up */;
-                bool bluetooth /* @text WAKEUPSRC_BLUETOOTH */ /* @brief Bluetooth Wakeup */;
-                bool wifi /* @text WAKEUPSRC_WIFI */ /* @brief WiFi Wake up*/;
-                bool ir /* @text WAKEUPSRC_IR */ /* @brief IR Remote Wake up */;
-                bool powerKey /* @text WAKEUPSRC_POWER_KEY */ /* @brief Power Button Wake up - GPIO */;
-                bool cec /* @text WAKEUPSRC_CEC */ /* @brief HDMI CEC command Wake up */;
-                bool lan /* @text WAKEUPSRC_LAN */ /* @brief LAN wake up */;
-                bool timer /* @text WAKEUPSRC_TIMER */ /* @brief Timer Wake up */;
+            enum WakeupSrcType : uint16_t {
+                WAKEUP_SRC_UNKNOWN          = 0    /* @text WAKEUPSRC_UNKNOWN */ /* @brief Voice Wake up */,
+                WAKEUP_SRC_VOICE            = 1    /* @text WAKEUPSRC_VOICE */ /* @brief Voice Wake up */,
+                WAKEUP_SRC_PRESENCEDETECTED = 2    /* @text WAKEUPSRC_PRESENCE_DETECTION */ /* @brief Presence detection wake up */,
+                WAKEUP_SRC_BLUETOOTH        = 3    /* @text WAKEUPSRC_BLUETOOTH */ /* @brief Bluetooth Wakeup */,
+                WAKEUP_SRC_WIFI             = 4    /* @text WAKEUPSRC_WIFI */ /* @brief WiFi Wake up*/,
+                WAKEUP_SRC_IR               = 5    /* @text WAKEUPSRC_IR */ /* @brief IR Remote Wake up */,
+                WAKEUP_SRC_POWERKEY         = 6    /* @text WAKEUPSRC_POWER_KEY */ /* @brief Power Button Wake up - GPIO */,
+                WAKEUP_SRC_TIMER            = 7    /* @text WAKEUPSRC_TIMER */ /* @brief Timer Wake up */,
+                WAKEUP_SRC_CEC              = 8    /* @text WAKEUPSRC_CEC */ /* @brief HDMI CEC command Wake up */,
+                WAKEUP_SRC_LAN              = 9    /* @text WAKEUPSRC_LAN */ /* @brief LAN wake up */,
+                WAKEUP_SRC_RF4CE            = 10   /* @text WAKEUPSRC_RF4CE */ /* @brief RF4CE wake up */
             };
 
+            struct EXTERNAL WakeupSources {
+                WakeupSrcType wakeupSource;
+                bool enabled;
+            };
 
             using ISystemServicesWakeupSourcesIterator = RPC::IIteratorType<WakeupSources, ID_SYSTEMSERVICES_WAKEUPSOURCES_ITERATOR>;
 
